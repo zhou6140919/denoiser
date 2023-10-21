4dd9158e7ff6eef0532aa1c24063ecabadec97c5
Tong Zhou <tongzhou@vt.edu>
2023-10-20 22:49:24 -0400

    update preprocess

--- preprocess.sh
+++ preprocess.sh
@@ -1,2 +1,4 @@
 python scripts/preprocess.py
 python scripts/split.py
+python scripts/train_dev_test.py --root_dir './datasets/processed_data/train_denoiser'
+python scripts/train_dev_test.py --root_dir './datasets/processed_data/eval_denoiser'
